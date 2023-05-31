41aeb2c438a8161401ea432c71a266108ea7e800
IronySuzumiya <811073137@qq.com>
2023-05-31 17:43:23 +0800

    ensure it works

--- compile.sh
+++ compile.sh
@@ -4,21 +4,7 @@ PINPATH="$ZSIMPATH/pin"
 LIBCONFIGPATH="$ZSIMPATH/libconfig"
 NUMCPUS=$(grep -c ^processor /proc/cpuinfo)
 
-
-
-if [ "$1" = "z" ]
-then
-	echo "Compiling only ZSim ..."
-        export PINPATH
-        export LIBCONFIGPATH
-        scons -j$NUMCPUS
-else
-	echo "Compiling all ..."
-	export LIBCONFIGPATH
-	cd $LIBCONFIGPATH
-	./configure --prefix=$LIBCONFIGPATH && make install
-	cd ..
-
-	export PINPATH
-	scons -j$NUMCPUS
-fi
+echo "Compiling only ZSim ..."
+export PINPATH
+export LIBCONFIGPATH
+scons -j$NUMCPUS
